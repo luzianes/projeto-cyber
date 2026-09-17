a27c87e466e8e3fc12c1f915edeb8e2f7974676a
daviruy61 <165973026+daviruy61@users.noreply.github.com>
2026-09-17 18:37:47 -0300

    teste

--- projeto/g3/settings.py
+++ projeto/g3/settings.py
@@ -59,17 +59,8 @@ TARGET_ENV = os.getenv('TARGET_ENV', 'dev')
 NOT_PROD = not TARGET_ENV.lower().startswith('prod')
 SECRET_KEY = env_required('SECRET_KEY')
 DEBUG = env_bool('DEBUG')
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    "projeto-cyber.onrender.com",
-]
+ALLOWED_HOSTS = [*]
 
-render_hostname = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
-
-if render_hostname:
-    ALLOWED_HOSTS.append(render_hostname)
-    
 CSRF_TRUSTED_ORIGINS = [
     "https://projeto-cyber.onrender.com",
 ]
