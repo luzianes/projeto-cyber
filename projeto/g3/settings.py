23e8c152784bc5416bf12a81ebc8e078f9912139
daviruy61 <165973026+daviruy61@users.noreply.github.com>
2026-09-17 18:19:59 -0300

    Add allowed hosts for deployment configuration

--- projeto/g3/settings.py
+++ projeto/g3/settings.py
@@ -59,6 +59,11 @@ TARGET_ENV = os.getenv('TARGET_ENV', 'dev')
 NOT_PROD = not TARGET_ENV.lower().startswith('prod')
 SECRET_KEY = env_required('SECRET_KEY')
 DEBUG = env_bool('DEBUG')
+ALLOWED_HOSTS = [
+    "projeto-cyber.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 if NOT_PROD:
